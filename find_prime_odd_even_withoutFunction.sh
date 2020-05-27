6aa7124c17a5efb29ff59336840b6ef354d58071
unknown <neerajkumar8546@gmail.com>
2020-05-27 06:53:07 +0530

    cal prime odd even and its average

--- find_prime_odd_even_withoutFunction.sh
+++ find_prime_odd_even_withoutFunction.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -x
 read -p "Enter last range : " last
 
+sumevennum=0
+sumoddnum=0
+sumprimenum=0
+
 for (( i=1;i<=last;i++ ))
 do
 	  #find even or odd number
 	  if [ $(($i%2)) -eq 0 ]
       then
           array_store_even[$i]=$i
+			sumevennum=$(($sumevennum+$i))
      else
          array_store_odd[$i]=$i
+			sumoddnum=$(($sumoddnum+$i))
      fi
 
 		#find prime number
@@ -23,13 +29,29 @@ do
       done
       if [ $flag -eq 0 ]; then
          array_store_prime[$i]=$i
+			sumprimenum=$(($sumprimenum+$i))
       fi
-
-
 done
 
+printf "\n"
+echo "1:- Even Number are between 1 to $last : " ${array_store_even[@]}
+printf "\n"
 
-echo "Even Number are: " ${array_store_even[@]}
-echo "Odd Number are: " ${array_store_odd[@]}
-echo "Prime Number are: " ${array_store_prime[@]}
+EvenAvg=$(($sumevennum / ${#array_store_even[@]}))
+echo "Average Of Even num are : "$EvenAvg
+printf "\n"
+
+echo "2:- Odd Number are between 1 to $last : " ${array_store_odd[@]}
+printf "\n"
+
+OddAvg=$(($sumoddnum / ${#array_store_odd[@]}))
+echo "Average Of Odd num are : "$OddAvg
+printf "\n"
+
+echo "3:- Prime Number are between 1 to $last : " ${array_store_prime[@]}
+printf "\n"
+
+PrimeAvg=$(($sumprimenum / ${#array_store_prime[@]}))
+echo "Average Of Even num are : "$PrimeAvg
+printf "\n"
 
